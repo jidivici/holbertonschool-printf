c85ba4d34718c14b83397f21523fea656429706f
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-26 14:44:00 +0100

    fix: struct for printf

--- main.h
+++ main.h
@@ -1,15 +1,31 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef POINTER_H
+#define POINTER_H
 
 #include <stdio.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 int _strlen(char *str);
-int _printf(const char *format, ...);
+int sum_them_all(const unsigned int n, ...);
+
+/**
+ * struct op_s - Structure pour associer un caractère à une fonction
+ * @op: Le caractère représentant le type à afficher
+ * @f: Pointeur de fonction pointe avec le va list adequate.
+ *
+ * Cette structure permet de lier un type de donnée print
+ * dans des fonctions variadiques comme print_all.
+ */
+typedef struct op_s
+{
+	char op;
+	void (*f)(va_list *);
+} op_t;
+
+void print_all(const char * const format, ...);
 void print_char(va_list *ap);
 void print_int(va_list *ap);
 void print_double(va_list *ap);
 void print_string(va_list *ap);
-void print_all(const char * const format, ...);
 
 #endif
